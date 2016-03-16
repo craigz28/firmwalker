a6d36c502dbd34199414a0afdad2c0d5784b25f8
craigz28 <craigz28@craigsmith.net>
2016-03-16 18:07:05 -0400

    Update firmwalker.sh

--- firmwalker.sh
+++ firmwalker.sh
@@ -156,4 +156,4 @@ msg "##################################### emails"
 grep -sEIorh '([[:alnum:]_.-]+@[[:alnum:]_.-]+?\.[[:alpha:].]{2,6})' "$@" $FIRMDIR | sort | uniq | tee -a $FILE
 
 #Perform static code analysis 
-eslint -c .eslintrc.json $FIRMDIR | tee -a $FILE
+eslint -c eslintrc.json $FIRMDIR | tee -a $FILE
